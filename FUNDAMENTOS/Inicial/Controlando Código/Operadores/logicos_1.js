313dab40a569e1553eecfc984870da921704ef35
Alexandre <alexandrecferreira11@gmail.com>
2023-12-14 20:55:06 -0300

    trilha_inicial|controlando_codigo|operadores|logicos_#01

---
+++ FUNDAMENTOS/Inicial/Controlando Código/Operadores/logicos_1.js
@@ -0,0 +1,13 @@
+const trabalho1 = true;
+const trabalho2 = true;
+
+const logicoAND = trabalho1 && trabalho2;
+const logicoXOR = trabalho1 != trabalho2;
+const logicoOR = trabalho1 || trabalho2;
+
+const logicoNOT = !logicoOR;
+
+console.log(logicoAND);
+console.log(logicoXOR);
+console.log(logicoOR);
+console.log(logicoNOT);
