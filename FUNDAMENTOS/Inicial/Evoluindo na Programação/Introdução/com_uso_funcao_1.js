f13171f9f14014c43d776aa21b85cbefd2c6d0c3
Alexandre <alexandrecferreira11@gmail.com>
2024-01-22 20:47:15 -0300

    trilha_inicial|evoluindo_na_programação|introdução|com_uso_função

---
+++ FUNDAMENTOS/Inicial/Evoluindo na Programação/Introdução/com_uso_funcao_1.js
@@ -0,0 +1,25 @@
+const d1 = 3
+const m1 = 'Maio'
+const a1 = 2024
+
+function exibirData1() {
+    console.log(`Dia: ${d1}`)
+    console.log(`Dia: ${m1}`)
+    console.log(`Dia: ${a1}`)
+    console.log(`${d1} de ${m1} de ${a1}`)
+}
+
+const d2 = 10
+const m2 = 'Dezembro'
+const a2 = 2020
+
+function exibirData2() {
+    console.log(`Dia: ${d2}`)
+    console.log(`Dia: ${m2}`)
+    console.log(`Dia: ${a2}`)
+    console.log(`${d2} de ${m2} de ${a2}`)
+}
+
+exibirData1()
+exibirData2()
+
