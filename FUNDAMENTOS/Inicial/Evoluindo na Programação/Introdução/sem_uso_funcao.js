e8606b1bcc87ecfd99ed9323b6a071209022a7c6
Alexandre <alexandrecferreira11@gmail.com>
2024-01-22 20:40:19 -0300

    trilha_inicial|evoluindo_na_programação|introdução|sem_uso_função

---
+++ FUNDAMENTOS/Inicial/Evoluindo na Programação/Introdução/sem_uso_funcao.js
@@ -0,0 +1,17 @@
+const d1 = 3
+const m1 = 'Maio'
+const a1 = 2024
+
+console.log(`Dia: ${d1}`)
+console.log(`Dia: ${m1}`)
+console.log(`Dia: ${a1}`)
+console.log(`${d1} de ${m1} de ${a1}`)
+
+const d2 = 10
+const m2 = 'Dezembro'
+const a2 = 2020
+
+console.log(`Dia: ${d2}`)
+console.log(`Dia: ${m2}`)
+console.log(`Dia: ${a2}`)
+console.log(`${d2} de ${m2} de ${a2}`)
