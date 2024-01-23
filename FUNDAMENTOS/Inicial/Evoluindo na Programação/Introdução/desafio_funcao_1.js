712ab92fb3d3c1e446c998e098ffcd2dd13805d2
Alexandre <alexandrecferreira11@gmail.com>
2024-01-22 21:14:33 -0300

    trilha_inicial|evoluindo_na_programação|introdução|desafio_função_1(correção)

--- FUNDAMENTOS/Inicial/Evoluindo na Programação/Introdução/desafio_funcao_1.js
+++ FUNDAMENTOS/Inicial/Evoluindo na Programação/Introdução/desafio_funcao_1.js
@@ -1,11 +1,11 @@
 function mediaSimples(num1, num2, num3){
     const result = (num1 + num2 + num3) / 3;
-    console.log(result);
+    console.log(`A média simples é: ${result}!`);
 }
 
 function mediaPonderada(num1, num2, num3){
     const result = (num1 * 1 + num2 * 2 + num3 * 3) / 6;
-    console.log(result);
+    console.log(`A média ponderada é: ${result}!`);
 }
 
 mediaSimples(1, 2, 3)
