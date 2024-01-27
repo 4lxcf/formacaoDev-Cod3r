9dc8aa512be5aef841167f6d8851d12154f5f896
Alexandre <alexandrecferreira11@gmail.com>
2024-01-27 18:09:09 -0300

    trilha_inicial|evoluindo_na_programação|introdução|desafio_função_#03(melhoria)

--- FUNDAMENTOS/Inicial/Evoluindo na Programação/Introdução/desafio_funcao_3.js
+++ FUNDAMENTOS/Inicial/Evoluindo na Programação/Introdução/desafio_funcao_3.js
@@ -3,46 +3,22 @@ const nota2 = Math.random() * 10
 const nota3 = Math.random() * 10
 
 function mediaMaioresNotas(nota1, nota2, nota3){
-    let notaAlta1, notaAlta2
-    if (nota1 > nota2) {
-        notaAlta1 = nota1
-        if (nota2 > nota3) {
-            notaAlta2 = nota2
-        } else {
-            notaAlta2 = nota3
-        }
+    if (nota1 <= nota2 && nota1 <= nota3) {
+        return (nota2 + nota3) / 2
+    } else if (nota2 <= nota1 && nota2 <= nota3) {
+        return (nota1 + nota3) / 2
     } else {
-        notaAlta1 = nota2
-        if (nota1 > nota3) {
-            notaAlta2 = nota1
-        } else {
-            notaAlta2 = nota3
-        }
+        return (nota2 + nota1) / 2
     }
-
-    let media = (notaAlta1 + notaAlta2) / 2
-    return media
 }
 
 function resultadoAluno(num) {
-    switch (Math.floor(num)) {
-        case 10:
-        case 9:
-        case 8:
-        case 7:
-            return 'Parabéns! Aluno APROVADO!'
-            break;
-        case 6:
-        case 5:
-        case 4:
-            return 'O aluno precisa estudar mais. Está na RECUPERAÇÃO!'
-        case 3:
-        case 2:
-        case 1:
-        case 0:
-            return 'Infelizmente, aluno REPROVADO!'
-        default:
-            break;
+    if (num >= 7) {
+        return 'Parabéns! Aluno APROVADO!'
+    } else if (num >= 4) {
+        return 'O aluno precisa estudar mais. Está na RECUPERAÇÃO!'
+    } else {
+        return 'Infelizmente, aluno REPROVADO!'
     }
 }
 
