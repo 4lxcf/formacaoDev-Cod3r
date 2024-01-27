eaa7e2db3fbc575fca3cdbf1db2eb296a11284ed
Alexandre <alexandrecferreira11@gmail.com>
2024-01-27 17:13:51 -0300

    trilha_inicial|controlando_codigo|estruturas_de_controle|desafio_switch

---
+++ FUNDAMENTOS/Inicial/Controlando Código/Estruturas de Controle/desafio_switch.js
@@ -0,0 +1,38 @@
+let conceito = 'A+'
+
+switch(conceito) {
+    case 'A+':
+        console.log('Nota: 10')
+        break
+    case 'A':
+        console.log('Nota: 9')
+        break
+    case 'B+':
+        console.log('Nota: 8')
+        break
+    case 'B':
+        console.log('Nota: 7')
+        break
+    case 'C+':
+        console.log('Nota: 6')
+        break
+    case 'C':
+        console.log('Nota: 5')
+        break
+    case 'D+':
+        console.log('Nota: 4')
+        break
+    case 'D':
+        console.log('Nota: 3')
+        break
+    case 'E+':
+        console.log('Nota: 2')
+        break
+    case 'E':
+        console.log('Nota: 1')
+        break
+    case 'F':
+        console.log('Nota: 0')
+        break
+    
+}
