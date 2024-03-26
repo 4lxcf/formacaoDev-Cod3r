7fdb497bd4c24daaeb7493250954f26218d8deb7
Alexandre <alexandrecferreira11@gmail.com>
2024-03-25 21:25:11 -0300

    trilha_inicial|funções_em_javascript|promises|promise_#04_part2

--- FUNDAMENTOS/Inicial/Funções em Javascript/Promises/promise_04.js
+++ FUNDAMENTOS/Inicial/Funções em Javascript/Promises/promise_04.js
@@ -9,3 +9,18 @@ setTimeout(function (){
         }, 2000)
     }, 2000)
 }, 2000)
+
+// Promise
+
+function esperarPor(tempo = 2000) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log('Executando promise...')
+            resolve()
+        }, tempo)
+    })
+}
+
+esperarPor()
+    .then(esperarPor)
+    .then(esperarPor)
