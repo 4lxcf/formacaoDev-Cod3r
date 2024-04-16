873b248b5a8095a4986d6d0286108535d05e8346
Alexandre <alexandrecferreira11@gmail.com>
2024-04-16 20:04:09 -0300

    trilha_inicial|funções_em_javascript|funções_&_POO|objeto_literal_#03

---
+++ FUNDAMENTOS/Inicial/Funções em Javascript/Funções&POO/objeto_literal_03.js
@@ -0,0 +1,15 @@
+const usuario1 = {
+    nome: 'Ana',
+    email: 'ana@ana.com.br',
+    senha: '1234',
+    dataCadastro: new Date(),
+}
+
+const usuario2 = {
+    nome: 'Pedro',
+    email: 'pedro@ped.com.br',
+    senha: '1234',
+    dataCadastro: new Date(),
+}
+
+console.log(usuario1, usuario2)
