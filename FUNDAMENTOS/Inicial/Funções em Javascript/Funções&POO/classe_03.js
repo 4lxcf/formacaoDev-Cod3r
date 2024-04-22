c03f2341ac48e06899703656e2349ad1eefea8d7
Alexandre <alexandrecferreira11@gmail.com>
2024-04-22 20:36:17 -0300

    trilha_inicial|funções_em_javascript|funções_&_POO|classe_#03_3

--- FUNDAMENTOS/Inicial/Funções em Javascript/Funções&POO/classe_03.js
+++ FUNDAMENTOS/Inicial/Funções em Javascript/Funções&POO/classe_03.js
@@ -3,24 +3,45 @@ class Data {
     #mes = 1
     #ano = 1970
     
-    exibirData() {
-        return `${this.#dia}/${this.#mes}/${this.#ano}`
-    }
+    // Getters
+    get dia() { return this.#dia }
+    get mes() { return this.#mes }
+    get ano() { return this.#ano }
+    
 
-    getDia() {
-        return this.#dia
-    }
-
-    setDia(novoDia) {
+    // Setters
+    set dia(novoDia) {
         if (novoDia >= 1 && novoDia <= 31){
             this.#dia = novoDia
         }
     }
+    
+    set mes(novoMes) {
+        if (novoMes >= 1 && novoMes <= 12){
+            this.#mes = novoMes
+        }
+    }
+    
+    set ano(novoAno) {
+        if (novoAno >= 1950 && novoAno <= 2024){
+            this.#ano = novoAno
+        }
+    }
+    exibirData() {
+        return `${this.#dia}/${this.#mes}/${this.#ano}`
+    }
+
+
 }
 
 const d1 = new Data()
 
 // console.log(d1.exibirData(), d1)
-d1.setDia(36)
+d1.dia = 10
+d1.mes = 4
+d1.ano = 2000
 
-console.log(d1.getDia())
+
+console.log(d1.dia)
+console.log(d1.mes)
+console.log(d1.ano)
