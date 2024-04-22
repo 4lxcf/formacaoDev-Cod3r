dd8c15a38c5c593dba5eb58c7c662d494d7a1167
Alexandre <alexandrecferreira11@gmail.com>
2024-04-22 20:28:03 -0300

    trilha_inicial|funções_em_javascript|funções_&_POO|classe_#03_2

--- FUNDAMENTOS/Inicial/Funções em Javascript/Funções&POO/classe_03.js
+++ FUNDAMENTOS/Inicial/Funções em Javascript/Funções&POO/classe_03.js
@@ -3,12 +3,24 @@ class Data {
     #mes = 1
     #ano = 1970
     
-
-    exibir() {
+    exibirData() {
         return `${this.#dia}/${this.#mes}/${this.#ano}`
+    }
+
+    getDia() {
+        return this.#dia
+    }
+
+    setDia(novoDia) {
+        if (novoDia >= 1 && novoDia <= 31){
+            this.#dia = novoDia
+        }
     }
 }
 
 const d1 = new Data()
 
-console.log(d1.exibir(), d1)
+// console.log(d1.exibirData(), d1)
+d1.setDia(36)
+
+console.log(d1.getDia())
