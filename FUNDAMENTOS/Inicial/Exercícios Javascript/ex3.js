945c083f79ab7ab50e511b37160ea5f434947dcf
Alexandre <alexandrecferreira11@gmail.com>
2024-04-23 21:56:45 -0300

    trilha_inicial|exercicios_em_javascript|exercicio_#03

--- FUNDAMENTOS/Inicial/Exercícios Javascript/ex3.js
+++ FUNDAMENTOS/Inicial/Exercícios Javascript/ex3.js
@@ -1,5 +1,14 @@
 /* Crie uma função que recebe um objeto como parâmetro e retorna um segundo objeto com as chaves e valores do primeiro objeto invertidas*/
 function trocaChaveEValor(objeto){
+    let result = {}
+    let conteudo = Object.entries(objeto)
+    
+    for (const iterator of conteudo) {
+        console.log(iterator)
+        result[iterator[1]] = iterator[0]
+    }
+    
+    return result
 }
 
 const objeto = {nome:"Maria", idade:"45", id:"8745"}
