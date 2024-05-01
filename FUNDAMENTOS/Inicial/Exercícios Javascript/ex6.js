2237aaafcd56b9c1bc659cbd2a601c6a14ae0814
Alexandre <alexandrecferreira11@gmail.com>
2024-05-01 10:02:57 -0300

    trilha_inicial|exercicios_em_javascript|exercicio_#06

--- FUNDAMENTOS/Inicial/Exercícios Javascript/ex6.js
+++ FUNDAMENTOS/Inicial/Exercícios Javascript/ex6.js
@@ -1,7 +1,9 @@
 /*Desenvolva uma função que recebe como parâmetro um objeto e retorne um array de arrays, onde cada elemento do array é um outro array contendo o par chave/valor de um atributo do objeto*/
 
 function transformaEmArray(objeto){
+    let result = Object.entries(objeto)
 
+    return result
 }
 
 
