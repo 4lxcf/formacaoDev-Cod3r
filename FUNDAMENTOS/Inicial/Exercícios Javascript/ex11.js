c890ddc2476ddb27ee679c70afdafc48489cf7e0
Alexandre <alexandrecferreira11@gmail.com>
2024-05-08 21:13:20 -0300

    trilha_inicial|exercicios_em_javascript|exercicio_#11

--- FUNDAMENTOS/Inicial/Exercícios Javascript/ex11.js
+++ FUNDAMENTOS/Inicial/Exercícios Javascript/ex11.js
@@ -1,16 +1,35 @@
 //Implemente uma função que recebe um array de objetos e uma propriedade e ordene o array de acordo com a propriedade
 
 function ordena(arr, atributo, crescente = true){
+    const comparacao = (a, b) => {
+        if (a[atributo] > b[atributo]) {
+            if (crescente === true) {
+                return 1
+            } else {
+                return -1
+            }
+        } else if (a[atributo] < b[atributo]) {
+            if (crescente === true) {
+                return -1
+            } else {
+                return 1
+            }
+        } else {
+            return 0
+        }
     }
 
+    return arr.sort(comparacao)
+}
 
-    const pessoas = [
-        {nome: "Ana", idade:30},
-        {nome: "Enzo", idade:5},
-        {nome: "Carlos", idade:98},
-        {nome: "Anésia", idade:94},
-    ]
 
-    console.log(ordena(pessoas, "idade"))
-    console.log(ordena(pessoas, "idade", false))
-    console.log(ordena(pessoas, "nome"))
+const pessoas = [
+    {nome: "Ana", idade:30},
+    {nome: "Enzo", idade:5},
+    {nome: "Carlos", idade:98},
+    {nome: "Anésia", idade:94},
+]
+
+console.log(ordena(pessoas, "idade"))
+console.log(ordena(pessoas, "idade", false))
+console.log(ordena(pessoas, "nome"))
