500ccf0e10e42bb7afd591bc66dd9c6274422ee0
Alexandre <alexandrecferreira11@gmail.com>
2024-06-25 15:45:16 -0300

    3-ESPECIALISTA|2-React_Next|1-Fundamentos_React|jsx_dentro_do_javascript

---
+++ 3-ESPECIALISTA/2-React_Next/1-Fundamentos_React/react-fundamentos/src/app/componente/trecho/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(){
+    const trechos = [
+        <div key="1">Trecho #01</div>,
+        <div key="2">Trecho #02</div>,
+        <div key="3">Trecho #03</div>,
+    ]
+
+    return trechos[1]
+}
