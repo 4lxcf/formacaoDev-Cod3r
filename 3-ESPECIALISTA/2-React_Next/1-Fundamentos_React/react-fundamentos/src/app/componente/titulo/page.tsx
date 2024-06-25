fda1795b6557f1cc068cb1d5f7b8a045996edf2f
Alexandre <alexandrecferreira11@gmail.com>
2024-06-25 15:36:29 -0300

    3-ESPECIALISTA|2-React_Next|1-Fundamentos_React|Componente_na_pratica

---
+++ 3-ESPECIALISTA/2-React_Next/1-Fundamentos_React/react-fundamentos/src/app/componente/titulo/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+    return (
+        <div>
+            <h1 className="text-red-500 text-6xl font-bold">Título</h1>
+            <h2 className="text-green-600 text-4xl">Subtítulo</h2>
+        </div>
+    )
+}
