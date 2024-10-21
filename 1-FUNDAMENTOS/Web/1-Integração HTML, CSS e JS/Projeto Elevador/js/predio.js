545cb89de1826ae09af473d5e66651db86ab947f
Alexandre <alexandrecferreira11@gmail.com>
2024-10-20 23:13:10 -0300

    1-FUNDAMENTOS|2-Trilha_Web|6-Projeto_Elevador|Movimentação_Bruta

--- 1-FUNDAMENTOS/Web/1-Integração HTML, CSS e JS/Projeto Elevador/js/predio.js
+++ 1-FUNDAMENTOS/Web/1-Integração HTML, CSS e JS/Projeto Elevador/js/predio.js
@@ -38,21 +38,36 @@
     });
   }
 
+  function obterTamanho() {
+    const terreo = document.querySelector('[andar="T"]');
+    return terreo.offsetHeight;
+  }
+
   criarPavimento();
 
   // -> ELEVADOR
 
   function criarElevador() {
     const fosso = document.querySelector(".fosso");
-    const terreo = document.querySelector('[andar="T"]');
+
     const elevador = document.createElement("div");
 
     elevador.classList.add("elevador");
 
     fosso.appendChild(elevador);
 
-    elevador.style.height = terreo.offsetHeight;
+    elevador.style.height = obterTamanho();
+  }
+
+  function moverElevador(andar) {
+    const numero = andar === "T" ? 0 : +andar;
+
+    const elevador = document.querySelector(".elevador");
+    const valorDeElevador = +elevador.style.bottom.replace("px", "");
+
+    elevador.style.bottom = valorDeElevador + numero * obterTamanho();
   }
 
   criarElevador();
+  moverElevador("0");
 })();
