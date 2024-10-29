056d24fb94bfed0d4ea5ef820abc813a613f3618
Alexandre <alexandrecferreira11@gmail.com>
2024-10-29 20:47:00 -0300

    1-FUNDAMENTOS|2-Trilha_Web|2-Fundamentos_NodeJS|3-Modulos_ECMAScript|Export_default_#01

---
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/ECMASCRIPT/calculadora-modular/calculos.js
@@ -0,0 +1,9 @@
+export default {
+  texto: "Sou do arquivo Calculos.js !",
+  soma: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+};
---
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/ECMASCRIPT/calculadora-modular/index.js
@@ -0,0 +1,9 @@
+import calculos from "./calculos.js";
+
+let x, y;
+x = 10;
+y = 8;
+
+console.log(calculos.texto);
+console.log(calculos.soma(x, y));
+console.log(calculos.sub(x, y));
