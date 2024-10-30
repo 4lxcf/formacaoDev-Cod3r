e8e6d1ec9eed2ef4b89cc0d0eb8d55a0350df073
Alexandre <alexandrecferreira11@gmail.com>
2024-10-29 21:27:00 -0300

    1-FUNDAMENTOS|2-Trilha_Web|2-Fundamentos_NodeJS|3-Modulos_ECMAScript|Desafio_modulo_receita

--- 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/ECMASCRIPT/exemplo-cozinha/index.js
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/ECMASCRIPT/exemplo-cozinha/index.js
@@ -1,3 +1,15 @@
 import cozinha from "./cozinha/index.js";
+import receitaDefault from "./receita.js";
+import { receita as receitaIndividualizada } from "./receita.js";
 
 console.log(cozinha);
+console.log(
+  receitaDefault(cozinha.listaDeAlimentos, cozinha.assar, cozinha.cortar)
+);
+console.log(
+  receitaIndividualizada(
+    cozinha.listaDeAlimentos,
+    cozinha.assar,
+    cozinha.cortar
+  )
+);
---
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/ECMASCRIPT/exemplo-cozinha/receita.js
@@ -0,0 +1,23 @@
+export function receita(ingredientes, acao1, acao2) {
+  let result = "";
+
+  for (const ingrediente of ingredientes) {
+    result += `${acao1(ingrediente)}, `;
+    result += `${acao2(ingrediente)}. `;
+  }
+  result += "Receita Finalizada!";
+
+  return result;
+}
+
+export default function (ingredientes, acao1, acao2) {
+  let result = "";
+
+  for (const ingrediente of ingredientes) {
+    result += `${acao1(ingrediente)}, `;
+    result += `${acao2(ingrediente)}. `;
+  }
+  result += "Receita Finalizada!";
+
+  return result;
+}
