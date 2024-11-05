eab54a1a27d0979891666ff728f8a3a09be43c8c
Alexandre <alexandrecferreira11@gmail.com>
2024-11-04 22:02:03 -0300

    1-FUNDAMENTOS|2-Trilha_Web|2-Fundamentos_NodeJS|4-Projetos|Projeto_1_modulo_luxon

--- 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto1/index.js
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto1/index.js
@@ -1,9 +1,33 @@
 import chalk from "chalk";
 import server from "server";
+import { DateTime } from "luxon";
 
 const { get } = server.router;
 
-server({ port: 8080 }, [get("/", (ctx) => "Hello World")]);
+function imprimirHora(cidade, hora) {
+  console.log(chalk.bgBlue(`Em ${cidade} são ${hora} horas !`));
+  return `A hora exata de ${cidade} é: [ ${hora} ]`;
+}
+
+server({ port: 8080 }, [
+  get("/", (ctx) => "Hello World"),
+  get("/roma", () => {
+    const h = DateTime.local({ zone: "Europe/Rome" }).toFormat("HH:mm:ss");
+    return imprimirHora("Roma", h);
+  }),
+  get("/luanda", () => {
+    const h = DateTime.local({ zone: "Africa/Luanda" }).toFormat("HH:mm:ss");
+    return imprimirHora("Luanda", h);
+  }),
+  get("/seoul", () => {
+    const h = DateTime.local({ zone: "Asia/Seoul" }).toFormat("HH:mm:ss");
+    return imprimirHora("Seoul", h);
+  }),
+  get("/cuiaba", () => {
+    const h = DateTime.local({ zone: "America/Cuiaba" }).toFormat("HH:mm:ss");
+    return imprimirHora("Cuiaba", h);
+  }),
+]);
 
 // const azul = (texto) => console.log(chalk.blue(texto));
 // const aviso = (texto) => console.log(chalk.bgYellow.bold(texto));
