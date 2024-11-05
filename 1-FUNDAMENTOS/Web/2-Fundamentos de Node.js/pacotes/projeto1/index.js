2d2dd43d4f54dceddf1d0311188f0bf3c9c1142b
Alexandre <alexandrecferreira11@gmail.com>
2024-11-04 21:43:59 -0300

    1-FUNDAMENTOS|2-Trilha_Web|2-Fundamentos_NodeJS|4-Projetos|Projeto_1_modulo_server

--- 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto1/index.js
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto1/index.js
@@ -1,9 +1,14 @@
 import chalk from "chalk";
+import server from "server";
 
-const azul = (texto) => console.log(chalk.blue(texto));
-const aviso = (texto) => console.log(chalk.bgYellow.bold(texto));
-const erro = (texto) => console.log(chalk.bgRed.bold.italic(texto));
+const { get } = server.router;
 
-azul("Sou um texto");
-aviso("TEXTO DE AVISO");
-erro("ABORTAR MISSÃO");
+server({ port: 8080 }, [get("/", (ctx) => "Hello World")]);
+
+// const azul = (texto) => console.log(chalk.blue(texto));
+// const aviso = (texto) => console.log(chalk.bgYellow.bold(texto));
+// const erro = (texto) => console.log(chalk.bgRed.bold.italic(texto));
+
+// azul("Sou um texto");
+// aviso("TEXTO DE AVISO");
+// erro("ABORTAR MISSÃO");
