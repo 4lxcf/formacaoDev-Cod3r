9e0f137b4ac4c7c1bf5f2a7f11200809d62f5133
Alexandre <alexandrecferreira11@gmail.com>
2024-11-05 22:32:35 -0300

    1-FUNDAMENTOS|2-Trilha_Web|2-Fundamentos_NodeJS|4-Projetos|Projeto_2_modulo_fs

--- 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto2/index.js
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto2/index.js
@@ -1,8 +1,16 @@
 import pensador from "pensador-scrap";
+import fs from "fs";
 
 const obj = await pensador.search({ query: "Platao", limit: 10 });
 
 const pensamentos = obj.success.thought;
 const numeroAleatorio = Math.floor(Math.random() * pensamentos.length);
+const pensamentoSelecionado = pensamentos[numeroAleatorio].content;
+const autor = pensamentos[numeroAleatorio].author;
 
-console.log(pensamentos[numeroAleatorio].content);
+const pensamentoSalvo = `=> O pensamento escolhido de ${autor} foi: "${pensamentoSelecionado}"\n`;
+
+fs.appendFileSync("frases.txt", pensamentoSalvo, "utf8");
+
+const texto = fs.readFileSync("frases.txt", "utf8");
+console.log(texto);
