b11bed46d719d677f0cd68e7d4b4e3f6158cfe5d
Alexandre <alexandrecferreira11@gmail.com>
2024-11-06 21:58:02 -0300

    1-FUNDAMENTOS|2-Trilha_Web|2-Fundamentos_NodeJS|4-Projetos|Projeto_3_salvando_dados_no_arquivo

--- 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto3/banco.js
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto3/banco.js
@@ -1,4 +1,17 @@
-let bancoDeDados = []; // Formato {nome, id}
+import fs from "fs";
+
+let bancoDeDados = _carregarDados(); // Formato {nome, id}
+
+function _carregarDados() {
+  const dadosJSON = fs.readFileSync("banco.json", "utf8");
+  const dados = JSON.parse(dadosJSON);
+  return dados;
+}
+
+export function persistirValores() {
+  const dadosJSON = JSON.stringify(bancoDeDados, null, 4);
+  fs.writeFileSync("banco.json", dadosJSON, "utf8");
+}
 
 export function salvar(nome, id) {
   bancoDeDados.push({ nome, id });
--- 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto3/index.js
+++ 1-FUNDAMENTOS/Web/2-Fundamentos de Node.js/pacotes/projeto3/index.js
@@ -1,13 +1,16 @@
-import { salvar, deletar, ler } from "./banco.js";
-
-salvar("Ana", 13);
-salvar("Felipe", 11);
-salvar("Carla", 1);
-salvar("Monize", 16);
+import { salvar, deletar, ler, persistirValores } from "./banco.js";
 
 console.log(ler());
+
+salvar("Ana", 313);
+salvar("Felipe", 411);
+salvar("Carla", 51);
+salvar("Monize", 616);
 
 deletar(13);
 deletar(1);
 
+console.log("<-------------------------------------------------->");
 console.log(ler());
+
+persistirValores();
