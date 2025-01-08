59e12c03b47ad8fade7cefb882c0db4b41017a2a
Alexandre <alexandrecferreira11@gmail.com>
2025-01-08 16:24:27 -0300

    1-FUNDAMENTOS|2-Trilha_Web|3-Projeto_Cifra_de_Cesar|2-Projeto|Seleção_com_Javascript

--- 1-FUNDAMENTOS/Web/3-Projeto Cifra de César/script.js
+++ 1-FUNDAMENTOS/Web/3-Projeto Cifra de César/script.js
@@ -1,1 +1,34 @@
-const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const alfabeto = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const selector = document.getElementById("deslocamento");
+
+for (let i = 0; i < alfabeto.length; i++) {
+  selector.innerHTML += `<option value="${i}">${alfabeto[i]}</option>`;
+}
