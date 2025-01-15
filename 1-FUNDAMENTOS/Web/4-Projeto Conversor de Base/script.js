8a80783bbf9fd9f041698d2a98ac59c5aade4dcf
Alexandre <alexandrecferreira11@gmail.com>
2025-01-15 18:20:34 -0300

    1-FUNDAMENTOS|2-Trilha_Web|4.Projeto_Conversor_de_Base|2.Projeto|Javascript #01

---
+++ 1-FUNDAMENTOS/Web/4-Projeto Conversor de Base/script.js
@@ -0,0 +1,43 @@
+const button = document.querySelector("#button");
+const number = document.querySelector("#number");
+const initial_base = document.querySelector("#initial-base");
+const target_base = document.querySelector("#target-base");
+const result = document.querySelector("#result");
+
+button.addEventListener("click", () => {
+  let convertedNumber;
+  const actualNumber = +number.value;
+  const targetBaseNumber = transformBaseNameToNumber(target_base.value);
+
+  if (target_base.value === initial_base.value) {
+    convertedNumber = actualNumber;
+  } else {
+    convertedNumber = actualNumber.toString(targetBaseNumber);
+  }
+
+  console.log(`CONVERTED_NUMBER: ${convertedNumber}`);
+});
+
+function transformBaseNameToNumber(baseName) {
+  switch (baseName) {
+    case "Decimal":
+      return 10;
+      break;
+
+    case "Hexadecimal":
+      return 16;
+      break;
+
+    case "Octal":
+      return 8;
+      break;
+
+    case "Binario":
+      return 2;
+      break;
+
+    default:
+      return 0;
+      break;
+  }
+}
