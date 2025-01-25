92a2facd6f092d37d48cb3667ec6489571cd2190
Alexandre <alexandrecferreira11@gmail.com>
2025-01-25 17:06:05 -0300

    1-FUNDAMENTOS|2-Trilha_Web|4.Projeto_Conversor_de_Base|2.Projeto|Comentarios

--- 1-FUNDAMENTOS/Web/4-Projeto Conversor de Base/script.js
+++ 1-FUNDAMENTOS/Web/4-Projeto Conversor de Base/script.js
@@ -8,12 +8,8 @@ button.addEventListener("click", () => {
   let convertedNumber;
   const actualNumber = number.value;
 
-  if (target_base.value === initial_base.value) {
-    convertedNumber = actualNumber;
-  } else {
-    const middleNumber = parseInt(actualNumber, initial_base.value);
-    convertedNumber = +middleNumber.toString(+target_base.value);
-  }
+  const middleNumber = parseInt(actualNumber, initial_base.value);
+  convertedNumber = +middleNumber.toString(+target_base.value);
 
   result.classList.remove("invisible");
   result.innerHTML = `${actualNumber}<sub>${initial_base.value}</sub> => ${convertedNumber}<sub>${target_base.value}</sub>`;
