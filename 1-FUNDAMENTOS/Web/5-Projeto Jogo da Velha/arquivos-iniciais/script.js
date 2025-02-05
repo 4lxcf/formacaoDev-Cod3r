bc9f5cffc175bcb74de0fa32108f548e5aded964
Alexandre <alexandrecferreira11@gmail.com>
2025-02-04 22:57:11 -0300

    1-FUNDAMENTOS|2-Trilha_Web|5-Projeto_Jogo_Da_Velha|2-Aplicacao_Basica|3-Logica_#02_incomplete

--- 1-FUNDAMENTOS/Web/5-Projeto Jogo da Velha/arquivos-iniciais/script.js
+++ 1-FUNDAMENTOS/Web/5-Projeto Jogo da Velha/arquivos-iniciais/script.js
@@ -4,6 +4,13 @@ class Player {
   }
 }
 
+class Action {
+  constructor(line, column) {
+    this.line = line;
+    this.column = column;
+  }
+}
+
 class TicTacToe {
   constructor(player1 = new Player("x"), player2 = new Player("o")) {
     this.player1 = player1;
@@ -17,6 +24,40 @@ class TicTacToe {
     return Array.from({ length: this.size }, () => Array(this.size).fill(null));
   }
 
+  play(action) {
+    if (this.#isValidAction(action)) {
+      this.#render(action);
+      this.#switchTurn();
+    } else {
+      console.log("AÇÃO INVÁLIDA");
+    }
+  }
+
+  #switchTurn() {
+    this.turn =
+      this.turn.symbol === this.player1.symbol ? this.player2 : this.player1;
+  }
+
+  #render(action) {
+    let { line, column } = action;
+
+    this.board[line - 1][column - 1] = this.turn.symbol;
+  }
+
+  #isValidAction(action) {
+    let { line, column } = action;
+
+    if (line <= 0 || column <= 0) {
+      return false;
+    }
+
+    if (line > this.size || column > this.size) {
+      return false;
+    }
+
+    return true;
+  }
+
   mapping() {
     let matrix = this.board
       .map((item) => item.map((position) => position ?? "-").join(" "))
@@ -27,4 +68,7 @@ class TicTacToe {
 }
 
 const jogoDaVelha = new TicTacToe();
+jogoDaVelha.play(new Action(1, 1));
+jogoDaVelha.play(new Action(1, 2));
+jogoDaVelha.play(new Action(2, 2));
 console.log(jogoDaVelha.mapping());
